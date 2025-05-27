2453f69e65f58fc1e41f73be4bba7474468e2cc3
enricodelazzari <enrico.delazzari@maize.io>
2025-05-27 14:27:34 +0200

    wip

--- src/CloudfrontCookiesServiceProvider.php
+++ src/CloudfrontCookiesServiceProvider.php
@@ -14,7 +14,6 @@ class CloudfrontCookiesServiceProvider extends PackageServiceProvider
         $package
             ->name('laravel-cloudfront-cookies')
             ->hasConfigFile();
-
     }
 
     public function packageBooted(): void
