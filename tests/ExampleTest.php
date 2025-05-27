33966a82d6e32fc0c2d8d5ece06d08945bdc0cab
enricodelazzari <enrico.delazzari@maize.io>
2025-05-27 13:56:43 +0200

    wip

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,17 +1,5 @@
 <?php
 
-use function Pest\Laravel\get;
-
 it('can test', function () {
     expect(true)->toBeTrue();
-
-    // $response = get(
-    //     uri: '/',
-    //     cookies: collect($cookies)
-    //         ->mapWithKeys(fn (string $c) => [
-    //             $c => fake()->word(),
-    //         ])
-    //         ->toArray(),
-    // );
-
 });
