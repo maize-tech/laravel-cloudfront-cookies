a041ef3c358a849bd17b42f7db5f1a3cd0deafa0
enricodelazzari <10452445+enricodelazzari@users.noreply.github.com>
2025-05-27 12:07:27 +0000

    Fix styling

--- src/CloudfrontCookiesServiceProvider.php
+++ src/CloudfrontCookiesServiceProvider.php
@@ -14,7 +14,7 @@ class CloudfrontCookiesServiceProvider extends PackageServiceProvider
         $package
             ->name('laravel-cloudfront-cookies')
             ->hasConfigFile();
-            
+
     }
 
     public function packageBooted(): void
